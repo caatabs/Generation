8915159520bf2abdc9cdeb84c5fceba68dc8fd8c
Caatabs <caatabs@hotmail.com>
2024-01-08 17:53:38 -0300

    feat(LibretaDeNotas): add displayMenu function.

---
+++ src/LibretaDeNotas/LibretaDeNotas.java
@@ -0,0 +1,18 @@
+package LibretaDeNotas;
+
+public class LibretaDeNotas {
+    /** Menú -
+     *
+     * Función que muestra en pantalla las diferentes acciones que puede realizar el cliente entorno a la libreta de notas.
+     * */
+    private static void displayMenu() {
+        System.out.println("¿Que desea realizar con respecto a la libreta de notas del curso?");
+        System.out.println("1.- Mostrar el promedio de notas por estudiante.");
+        System.out.println("2.- Mostrar la evaluación correspondiente del alumno.");
+        System.out.println("3.- Mostrar si el promedio final del alumno es inferior o superior al promedio del curso.");
+        System.out.println("4.- Salir del menú.");
+    }
+    public static void main(String[] args) {
+
+    }
+}
