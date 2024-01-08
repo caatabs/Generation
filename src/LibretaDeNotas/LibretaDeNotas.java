908d7fe83639e42d1ec485c9826ec27c9970b51b
Caatabs <caatabs@hotmail.com>
2024-01-08 18:11:35 -0300

    feat(LibretaDeNotas): add promedioNotas function.

--- src/LibretaDeNotas/LibretaDeNotas.java
+++ src/LibretaDeNotas/LibretaDeNotas.java
@@ -1,5 +1,9 @@
 package LibretaDeNotas;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LibretaDeNotas {
     /** Menú -
      *
@@ -12,6 +16,29 @@ public class LibretaDeNotas {
         System.out.println("3.- Mostrar si el promedio final del alumno es inferior o superior al promedio del curso.");
         System.out.println("4.- Salir del menú.");
     }
+
+    /** Promedio de notas -
+     *
+     * Función que calcula el promedio de cada alumno en base a sus notas.
+     * @return HashMap cuya clave es el nombre del alumno y su valor asociado es el promedio de este.*/
+    private static Map promedioNotas(Map<String, List<Integer>> libretaDeNotas, Integer cantidadNotas) {
+        // HashMap en el cual se almacenara el nombre del alumno junto con su promedio de notas.
+        Map<String, Integer> promedioAlumno = new HashMap<>();
+
+        for (String nombreAlumno : libretaDeNotas.keySet()){
+            List<Integer> notas = libretaDeNotas.get(nombreAlumno);
+            Integer sumaNotas = 0, promedio;
+
+            for (Integer nota : notas) {
+                sumaNotas += nota;
+            }
+            promedio = sumaNotas / cantidadNotas;
+            promedioAlumno.put(nombreAlumno, promedio);
+        }
+
+        return promedioAlumno;
+    }
+
     public static void main(String[] args) {
 
     }
