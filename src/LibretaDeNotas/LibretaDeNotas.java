936cdc64520771e93d60599173676a97f3609cfb
Caatabs <caatabs@hotmail.com>
2024-01-09 10:01:58 -0300

    refactor(displayMenu):  more aesthetic menu

--- src/LibretaDeNotas/LibretaDeNotas.java
+++ src/LibretaDeNotas/LibretaDeNotas.java
@@ -2,18 +2,26 @@ package LibretaDeNotas;
 
 import java.util.*;
 
+// TODO Hacer más bonito el menú. [DONE]
+// TODO Implementar validaciones.
+// TODO Hacer funciones que busquen la nota mínima y máxima de un alumno.
+
 public class LibretaDeNotas {
     /** Menú -
      *
      * Función que muestra en pantalla las diferentes acciones que puede realizar el cliente entorno a la libreta de notas.
      * */
     private static void displayMenu() {
-        System.out.println("¿Que acción desea realizar con respecto a la libreta de notas del curso?");
-        System.out.println("1.- Mostrar el promedio de notas por estudiante.");
-        System.out.println("2.- Mostrar el libro de notas del curso.");
-        System.out.println("3.- Mostrar la evaluación correspondiente del alumno.");
-        System.out.println("4.- Mostrar si el promedio final del alumno es inferior o superior al promedio del curso.");
-        System.out.println("5.- Salir del menú.");
+        System.out.println("                                                                                             ");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("| ¿Que acción desea realizar con respecto a la libreta de notas del curso?                  |");
+        System.out.println("| 1.- Mostrar el promedio de notas por estudiante.                                          |");
+        System.out.println("| 2.- Mostrar el libro de notas del curso.                                                  |");
+        System.out.println("| 3.- Mostrar la evaluación correspondiente del alumno.                                     |");
+        System.out.println("| 4.- Mostrar si el promedio final del alumno es inferior o superior al promedio del curso. |");
+        System.out.println("| 5.- Salir del menú.                                                                       |");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("                                                                                             ");
     }
 
     /** Promedio de notas -
