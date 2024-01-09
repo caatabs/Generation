637d5d35f90b69e884fb22f2d655f42b531ba774
Caatabs <caatabs@hotmail.com>
2024-01-08 22:47:06 -0300

    feat(libretaDeNotas): add compararNota, promedioCurso functions.

--- src/LibretaDeNotas/LibretaDeNotas.java
+++ src/LibretaDeNotas/LibretaDeNotas.java
@@ -19,7 +19,9 @@ public class LibretaDeNotas {
     /** Promedio de notas -
      *
      * Función que calcula el promedio de cada alumno en base a sus notas.
-     * @return HashMap cuya clave es el nombre del alumno y su valor asociado es el promedio de este.*/
+     * @param libretaDeNotas Libro de las notas de cada alumno existente en el curso.
+     * @param cantidadNotas Valor ingresado inicialmente el cual representa la cantidad de notas que tiene cada alumno.
+     * @return HashMap que retorna pares alumno-promedio como clave-valor..*/
     private static Map<String, Double> promedioNotas(Map<String, List<Integer>> libretaDeNotas, Integer cantidadNotas) {
         // HashMap en el cual se almacenara el nombre del alumno junto con su promedio de notas.
         Map<String, Double> promedioAlumno = new HashMap<>();
@@ -40,17 +42,24 @@ public class LibretaDeNotas {
             promedioAlumno.put(nombreAlumno, promedio);
         }
 
+        return promedioAlumno;
+    }
+
+    /** Libreta de promedios -
+     *
+     * Función que muestra en pantalla el nombre de los alumnos junto con su respectivo promedio.
+     * @param promedioCurso HashMap cuyos pares clave-valor corresponden a alumno-promedio.*/
+    private static void libretaDePromedios(Map<String, Double> promedioCurso) {
         // Muestra en pantalla los alumnos junto con sus respectivos promedios.
-        for (Map.Entry<String, Double> entry : promedioAlumno.entrySet()) {
+        for (Map.Entry<String, Double> entry : promedioCurso.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-
-        return promedioAlumno;
     }
 
     /** Libreta de notas -
      *
      * Función que muestra en pantalla los datos existentes en la libreta de notas
+     * @param libretaDeNotas Libro de notas del curso actual.
      * */
     private static void libretaDeNotas(Map<String, List<Integer>> libretaDeNotas) {
         for (Map.Entry<String, List<Integer>> entry : libretaDeNotas.entrySet()) {
@@ -61,6 +70,7 @@ public class LibretaDeNotas {
     /** Evaluación del alumno -
      *
      * A partir del nombre del alumno ingresado se evalúa si su promedio es aprobado o reprobado
+     * @param promedioCurso HashMap cuyo pares clave-valor corresponden a alumno-promedio.
      */
     private static void evaluacion(Map<String, Double> promedioCurso) {
         Scanner scannerAlumno = new Scanner(System.in);
@@ -71,19 +81,24 @@ public class LibretaDeNotas {
         Double promedioAlumno = promedioAlumno(promedioCurso, nombreAlumno);
 
         if (promedioAlumno >= 0 && promedioAlumno <= 3) {
-            System.out.println("La calificación final del alumno " + nombreAlumno +" es 'No aprobado'.");
+            System.out.println("La calificación final del alumno " + nombreAlumno +" es " + promedioAlumno +
+                    ", por lo que su nota final es 'No aprobado'.");
         }
         else if (promedioAlumno > 3 && promedioAlumno <= 5) {
-            System.out.println("La calificación final del alumno " + nombreAlumno +" es 'Insuficiente'.");
+            System.out.println("La calificación final del alumno " + nombreAlumno +" es " + promedioAlumno +
+                    ", por lo que su nota final es 'Insuficiente'.");
         }
         else if (promedioAlumno > 5 && promedioAlumno <= 8) {
-            System.out.println("La calificación final del alumno " + nombreAlumno +" es 'Aceptable'.");
+            System.out.println("La calificación final del alumno " + nombreAlumno +" es " + promedioAlumno +
+                    ", por lo que su nota final es 'Aceptable'.");
         }
         else if (promedioAlumno > 8 && promedioAlumno < 10) {
-            System.out.println("La calificación final del alumno " + nombreAlumno +" es 'Sobresaliente'.");
+            System.out.println("La calificación final del alumno " + nombreAlumno +" es " + promedioAlumno +
+                    ", por lo que su nota final es 'Sobresaliente'.");
         }
         else if (promedioAlumno == 10) {
-            System.out.println("La calificación final del alumno " + nombreAlumno +" es 'Excelente'.");
+            System.out.println("La calificación final del alumno " + nombreAlumno +" es " + promedioAlumno +
+                    ", por lo que su nota final es 'Excelente'.");
         }
         else {
             System.out.println("Entrada inválida");
@@ -96,21 +111,58 @@ public class LibretaDeNotas {
      * @param promediosAlumnos HashMap del promedio de cada uno de los alumnos del curso.
      * @param alumno Nombre del alumno al cual se le quiere buscar su promedio.
      *
-     * @return El promedio del alumno. En caso de no existir muestra en pantalla un mensaje de error y retorna null*/
+     * @return El promedio del alumno. En caso de no existir muestra en pantalla un mensaje de error y retorna null.*/
     private static Double promedioAlumno(Map<String, Double> promediosAlumnos, String alumno) {
         Double promedio = promediosAlumnos.get(alumno);
         if (promedio != null) {
             return promedio;
         }
         else {
-            System.out.println("El alumno " + alumno + "no existe en el curso.");
+            System.out.println("El alumno " + alumno + " no existe en el curso.");
             return null;
+        }
+    }
+
+    /** Promedio del curso -
+     *
+     * Función que calcula el promedio actual del curso.
+     * @param promediosAlumnos HashMap del promedio de cada uno de los alumnos del curso.
+     * @param cantidadAlumnos Valor ingresado inicialmente el cual representa la cantidad de alumnos presentes en el curso.
+     * */
+    protected static Double promedioCurso(Map<String, Double> promediosAlumnos, Integer cantidadAlumnos) {
+        Double promedio, sumaNotas = 0.0;
+
+        for (String nombreAlumno : promediosAlumnos.keySet()){
+            sumaNotas += promediosAlumnos.get(nombreAlumno);
+        }
+        promedio = sumaNotas / cantidadAlumnos;
+
+        return promedio;
+    }
+
+    protected static void compararNota(Map<String, Double> promedioCurso, Integer cantidadAlumnos){
+        Scanner scannerAlumno = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del alumno: ");
+        String nombreAlumno = scannerAlumno.nextLine();
+
+        Double promedioAlumno = promedioAlumno(promedioCurso, nombreAlumno);
+        Double promedioClase = promedioCurso(promedioCurso, cantidadAlumnos);
+
+        if (promedioAlumno > promedioClase) {
+            System.out.println("El promedio del alumno " + nombreAlumno + " es superior al promedio del curso.");
+        }
+        else if (promedioAlumno == promedioClase) {
+            System.out.println("El promedio del alumno " + nombreAlumno + " es igual al promedio del curso.");
+        }
+        else {
+            System.out.println("El promedio del alumno " + nombreAlumno + " es inferior al promedio del curso.");
         }
     }
 
     /** Main -
      *
-     * Entrada al programa */
+     * Entrada al programa. */
     public static void main(String[] args) {
         Integer cantidadAlumnos;
         Integer cantidadNotas;
@@ -155,20 +207,20 @@ public class LibretaDeNotas {
             displayMenu();
             chosenOption= Integer.parseInt(scanner.nextLine());
 
+            Map<String, Double> promedioCurso = promedioNotas(libretaDeNotas, cantidadNotas);
+
             switch (chosenOption) {
                 case 1:
-                    promedioNotas(libretaDeNotas, cantidadNotas);
+                    libretaDePromedios(promedioCurso);
                     break;
                 case 2:
                     libretaDeNotas(libretaDeNotas);
                     break;
                 case 3:
-                    Map<String, Double> promedioCurso = promedioNotas(libretaDeNotas, cantidadNotas);
-
+                    evaluacion(promedioCurso);
                     break;
                 case 4:
-                    System.out.println("\\ comparacion promedio curso //");
-
+                    compararNota(promedioCurso, cantidadAlumnos);
                     break;
                 case 5:
                     System.out.println("¡Gracias por utilizar nuestro programa!," +
@@ -181,6 +233,5 @@ public class LibretaDeNotas {
 
 
         scanner.close();
-
     }
 }
